084e030d5755d9a292158b13ac466311bec7d263
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-02 16:40:15 +0700

    Writing Spring Data JPA entities & repositories to interact with DB tables

---
+++ src/main/java/com/arbi/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.arbi.accounts.repository;
+
+import com.arbi.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
