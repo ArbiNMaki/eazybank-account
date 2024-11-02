5c1f2962ba5d994a0acb46bc19e19252b5f7f88d
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-02 20:35:39 +0700

    CREATE API inside accounts microservice - Part 2

--- src/main/java/com/arbi/accounts/controller/AccountsController.java
+++ src/main/java/com/arbi/accounts/controller/AccountsController.java
@@ -3,6 +3,9 @@ package com.arbi.accounts.controller;
 import com.arbi.accounts.constants.AccountsConstants;
 import com.arbi.accounts.dto.CustomerDto;
 import com.arbi.accounts.dto.ResponseDto;
+import com.arbi.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api",
                 produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
-
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto
