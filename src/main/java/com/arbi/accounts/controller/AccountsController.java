091456a7f23bf6a5a20d9221d3cee03e9a0f9bac
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-04 16:10:37 +0700

    Handle all types of runtime exceptions using global logic inside accounts

--- src/main/java/com/arbi/accounts/controller/AccountsController.java
+++ src/main/java/com/arbi/accounts/controller/AccountsController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/api",
                 produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 public class AccountsController {
 
     private IAccountsService iAccountsService;
